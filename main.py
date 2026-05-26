23900b19431ed22999fafb30dfba085c57089801
BrianTanNguyen <167112896+BrianTanNguyen@users.noreply.github.com>
2026-05-27 00:10:56 +0700

    feat: Add main.py script and Dockerfile for containerization

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import platform
+def main():
+    print(f"Welcome to the lab!")
+    print(f"python version: {sys.version}")
+    print(f"platform: {platform.platform()}")
+if __name__ == "__main__":
+    main()
